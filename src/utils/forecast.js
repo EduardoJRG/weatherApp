821dd209a46f2ae36e5eff0aa942959478c40605
EduardoR <eduardojrg86@gmail.com>
2020-05-08 17:15:39 -0300

    change api forecast and unity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url =  `https://api.darksky.net/forecast/68db5af343b1a89c046ae07c8b1226a0/${latitude},${longitude}`
+    const url =`http://api.weatherstack.com/current?access_key=7465ebeb3c5a59f4d26856b91eeb323c&query=${latitude},${longitude}&units=m`
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,9 +9,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, ' It is currently '  + body.current.temperature + ' celsius degress out. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
 }
 
 module.exports = forecast
+
